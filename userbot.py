9bb24ad123454373b3231ae71ff63cb9243f1e34
Christian De Nisi <denisichristian05@libero.it>
2021-10-14 17:13:07 +0200

    changes

--- userbot.py
+++ userbot.py
@@ -1,17 +1,14 @@
-from telethon.sync import TelegramClient, events
-import asyncio
+from dotenv import dotenv_values
+from telethon.sync import TelegramClient
 
-api_id = 8339070
-api_hash = "f65352465cb4bc25779336627a19112d"
-
-loop = asyncio.get_event_loop()
+config = dotenv_values(".env")
+api_id = config['API_ID']
+api_hash = config['API_HASH']
 
 with TelegramClient('name', api_id, api_hash) as client:
 
-    async def send_multiple_message():
-        while True:
-            await client.send_message('me', 'Hello, myself!')
-            await asyncio.sleep(3)
+    def send_multiple_message():
+        client.send_message('@lupen1324', 'Hello, myself!')
+        # await asyncio.sleep(60*3)
 
-    loop.create_task(send_multiple_message())
     client.run_until_disconnected()
