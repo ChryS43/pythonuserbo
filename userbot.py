bc5d025457146b374f4622c96b862f7525f7f1a7
Christian De Nisi <denisichristian05@libero.it>
2021-10-14 17:56:04 +0200

    changes

--- userbot.py
+++ userbot.py
@@ -5,11 +5,18 @@ config = dotenv_values(".env")
 api_id = config['API_ID']
 api_hash = config['API_HASH']
 
+spam_group_list = None
+
+with open('spam_groups.txt', 'r') as f:
+    spam_group_list = f.readlines()
+
 with TelegramClient('name', api_id, api_hash) as client:
 
     def send_multiple_message():
-        client.send_message('@lupen1324', 'Hello, myself!')
-        # await asyncio.sleep(60*3)
+
+        for spam_group in spam_group_list:
+            client.send_message(spam_group, 'https://t.me/cashin_guadagni_passivi')
+        exit()
 
     send_multiple_message()
 
